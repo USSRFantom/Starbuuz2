13c936b98f160d744e1f864f6a9736edb459b6a5
Дмитрий <dimkakrohin@mail.ru>
2019-10-16 22:37:43 +0300

    Initial commit

---
+++ app/src/main/java/com/hfad/starbuuz/TopLevelActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.starbuuz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+    }
+}
