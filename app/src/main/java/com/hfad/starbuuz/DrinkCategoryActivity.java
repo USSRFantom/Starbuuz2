ac47ef4a9ac7df300c908eac69dba6305e761e43
Дмитрий <dimkakrohin@mail.ru>
2019-10-16 23:30:56 +0300

    sleep

---
+++ app/src/main/java/com/hfad/starbuuz/DrinkCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.starbuuz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrinkCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+    }
+}
--- app/src/main/java/com/hfad/starbuuz/TopLevelActivity.java
+++ app/src/main/java/com/hfad/starbuuz/TopLevelActivity.java
@@ -2,7 +2,11 @@ package com.hfad.starbuuz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                if (position ==0){
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                }
+            }
+        };
+        ListView listView = (ListView) findViewById(R.id.list_optoins);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
